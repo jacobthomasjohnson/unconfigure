6aac31c1bb27eae628ede5a85a572d2f9f23192f
jacobthomasjohnson <jacobjohnson@email.com>
2025-06-06 18:46:42 +0000

    Added favicon.svg, icon_192.png, and icon_512.png. Working to add PWA functionality, may not be complete.

--- app/layout.js
+++ app/layout.js
@@ -2,15 +2,27 @@ import './globals.css'
 
 export const metadata = {
   title: 'Unconfigure - Reorder it',
-  description: 'A game where you guess the correct order of items'
+  description: 'A game where you guess the correct order of items',
+  icons: {
+    icon: '/favicon.svg'
+  }
 }
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icon_192.png" />
+        <meta name="theme-color" content="#121413" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      </head>
       <body className='antialiased overflow-auto h-[100svh]'>
         <div className='flex flex-col px-4'>{children}</div>
       </body>
-    </html>
+    </html >
   )
 }
